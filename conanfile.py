8ef469c868aff40059c5e85754b21c0351882b54
Harada, Tomoyuki <harada.tomoyuki@jp.fujitsu.com>
2017-02-27 16:04:26 +0900

    append library path to LD_LIBRARY_PATH in Linux

--- conanfile.py
+++ conanfile.py
@@ -12,21 +12,11 @@ class ZmqppConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake", "txt", "env"
     exports = "CMakeLists.txt"
-    options = {"shared": [True, False],
-               "zmqpp_libzmq_cmake": [True, False],
-               "zmqpp_build_examples": [True, False],
-               "zmqpp_build_client": [True, False],
-               "zmqpp_build_tests": [True, False]}
-    default_options = '''
-shared=False
-zmqpp_libzmq_cmake=False
-zmqpp_build_examples=False
-zmqpp_build_client=False
-zmqpp_build_tests=False
-'''
+    options = {"shared": [True, False]}
+    default_options = "shared=True"
 
     def requirements(self):
-        self.requires("libzmq/4.1.5@memsharded/stable")
+        self.requires("libzmq/4.1.5@gasuketsu/testing")
 
     def source(self):
        self.run("git clone https://github.com/zeromq/zmqpp.git")
@@ -36,17 +26,11 @@ zmqpp_build_tests=False
 
     def build(self):
         cmake = CMake(self.settings)
-        cmake_options = []
-        for option_name in self.options.values.fields:
-            activated = getattr(self.options, option_name)
-            the_option = "%s=" % option_name.upper()
-            if option_name == "shared":
-                the_option = "ZMQPP_BUILD_SHARED=ON" if activated else "ZMQPP_BUILD_SHARED=OFF"
-            else:
-                the_option += "ON" if activated else "OFF"
-            cmake_options.append(the_option)
+        cmake_options = ["-DZMQPP_BUILD_CLIENT=ON"]
+        shared_option = "ZMQPP_BUILD_STATIC=OFF" if self.options.shared else "ZMQPP_BUILD_SHARED=OFF"
+        cmake_options.append(shared_option)
         options_zmqpp = " -D".join(cmake_options)
-        conf_command = 'cd zmqpp/cmake_build && cmake .. %s -D%s' % (cmake.command_line, options_zmqpp)
+        conf_command = 'cd zmqpp/cmake_build && cmake .. %s %s' % (cmake.command_line, options_zmqpp)
         self.output.warn(conf_command)
         self.run("mkdir -p zmqpp/cmake_build")
         self.run(conf_command)
@@ -54,10 +38,13 @@ zmqpp_build_tests=False
 
     def package(self):
         self.copy("*.hpp", dst="include/zmqpp", src="zmqpp/src/zmqpp")
-        self.copy("*.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
+        self.copy("*.lib", dst="lib", src="zmqpp/cmake_build", keep_path=False)
+        self.copy("*.dll", dst="bin", src="zmqpp/cmake_build", keep_path=False)
+        self.copy("*.so", dst="lib", src="zmqpp/cmake_build", keep_path=False, symlinks=True)
+        self.copy("*.a", dst="lib", src="zmqpp/cmake_build", keep_path=False)
+        self.copy("*", dst="bin", src="zmqpp/cmake_build/bin", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = ["libzmqpp.so"] if self.options.shared else ["libzmqpp-static.a"]
+        self.cpp_info.libs = ["zmqpp"] if self.options.shared else ["zmqpp-static"]
+        self.env_info.path.append(os.path.join(self.package_folder, "bin"))
+        self.env_info.LD_LIBRARY_PATH.append(os.path.join(self.package_folder, "lib"))
